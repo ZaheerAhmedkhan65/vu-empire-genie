872563130a2b72071f65bff4844882b4f352a718
ZaheerAhmedkhan65 <ranazaheerahmed65@gmail.com>
2026-02-12 22:30:04 +0500

    resolved conflicts from server

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,7 @@
-<<<<<<< Updated upstream
-//server.js
-=======
->>>>>>> Stashed changes
 const app = require('./config/app');
 require("dotenv").config();
 const PORT = process.env.PORT;
 
-
 app.listen(PORT, () => {
-<<<<<<< Updated upstream
     console.log(`Server is running on http://localhost:${PORT}`);
-    console.log(`Health check: http://localhost:${PORT}/health`);
-    console.log(`API endpoint: http://localhost:${PORT}/api/quiz/save`);
-=======
-    console.log(`Server is running on port ${PORT}`);
-    console.log(`Health check: http://localhost:${PORT}/health`);
-    console.log(`API endpoint: http://localhost:${PORT}/api/quiz/save-bulk`);
->>>>>>> Stashed changes
 });
